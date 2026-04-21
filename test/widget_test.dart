56ea67e663b15a925b5244c4825bdcfbf3e5b070
NazZqi <mendoza.ig.an@gmail.com>
2026-04-21 19:44:28 -0400

    Restringe acceso no autenticado y gestiona sesion por politicas

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ruteando_app/main.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   testWidgets('muestra el boton de registro y abre la pantalla de registro', (
     WidgetTester tester,
   ) async {
+    SharedPreferences.setMockInitialValues({});
+
     await tester.pumpWidget(const RuteandoApp());
+    await tester.pumpAndSettle();
 
     expect(find.text('Registrarme'), findsOneWidget);
 
